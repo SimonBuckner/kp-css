dbff64c9ac96b3a2778856af03b1e5d8b20b3f1e
Simon Buckner <simonbuckner@hotmail.com>
2024-03-09 12:50:09 +0000

    Finish Navbar component
    - Separate templte creation from setting the active item
    - Fix issue with generating the a hrefs
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/index.html
    	modified:   src/js/navbar.js
    	modified:   src/pages/flex-columns.html
    	modified:   src/pages/hero-image.html
    	modified:   src/pages/hero-text.html

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,39 +1,44 @@
 const navPages = [
+    ["index", "Home", "home"],
     ["pages/hero-text", "Hero Text", "hero-text"],
     ["pages/flex-columns", "Flex Columns", "flex-columns"],
     ["pages/hero-image", "Hero Image", "hero-image"],
 ];
 
-
-
-function loadNavbar(pageName) {
-    console.log("load nav bar");
-    let content = `<ul>\n`;
-    navPages.forEach((item) => {
-        let activeClass = "";
-        if (pageName === item[2]) {
-            activeClass = "class=\"navbar__item--active\"";
-        }
-        let link = `<li><a href=\"${item[0]}.html\" ${activeClass}>${item[1]}</a></li>\n`;
-        content = content + link;
-    });
-    content = content + `</ul>\n`;
-    
-    class Navbar extends HTMLElement {
-        constructor() {
-            super();
-        }
-        
-        connectedCallback() {
-            this.innerHTML = content;
-            console.log("nav-bar connected");
-        }
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
     }
     
-    customElements.define('nav-bar', Navbar);
+    connectedCallback() {
+        console.log("connected nav bar");
+        let content = `<ul>\n`;
+        navPages.forEach((item) => {
+            let link = `<li><a href="\\${item[0]}.html" id="navbar__${item[2]}">${item[1]}</a></li>\n`;
+            content = content + link;
+        });
+        content = content + `</ul>\n`;
+        this.innerHTML = content
+    }
+}
+
+customElements.define('nav-bar', Navbar);
+
+function setActivePage(pageName) {
+    console.log("load nav bar");
     
+    let item = document.getElementById("navbar__" + pageName)
+    console.log(item)
+    if (item != null) {
+        item.setAttribute("class", "navbar__item--active")
+    }
+    // let activeClass = "";
+    // if (pageName === item[2]) {
+    //     activeClass = "class=\"navbar__item--active\"";
+    // }
+
 }
 
 
-export { loadNavbar, navPages };
-window.loadNavbar = loadNavbar;
+export { setActivePage };
+window.setActivePage = setActivePage;
